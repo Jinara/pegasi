728a8e13543dbd4b95c99b63c0d078748670c692
Nathaly Villamor <nathalyvillamor6@gmail.com>
2024-04-02 16:33:09 -0300

    adding basic model for user

---
+++ src/models/user.ts
@@ -0,0 +1,17 @@
+import * as mongodb from "mongodb";
+
+export interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+  sex: "female" | "male";
+  pregnancy: boolean;
+  birthdate: Date;
+  phoneNumber: string;
+
+  _id?: mongodb.ObjectId;
+}
+
+
+//nathalyvillamor6
+//eilsktw8WX7sBMoz
